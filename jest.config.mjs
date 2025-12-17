4f06111283c61e831f5d357abe8965393e27785f
Deepak Kumar <doomerdpk@gmail.com>
2025-12-17 16:14:23 +0530

    added tests

---
+++ jest.config.mjs
@@ -0,0 +1,10 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({ dir: './' });
+
+const customJestConfig = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
+
+module.exports = createJestConfig(customJestConfig);
